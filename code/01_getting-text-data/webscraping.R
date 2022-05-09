10fa81d88e6c35e525f51c7803fb6971876f160c
joeornstein <jornstein@gmail.com>
2022-05-09 17:03:48 -0400

    rvest tutorial

--- code/01_getting-text-data/webscraping.R
+++ code/01_getting-text-data/webscraping.R
@@ -7,20 +7,34 @@
 
 # load packages
 library(tidyverse)
+library(tidytext)
 library(rvest) # for harvesting text data from the web
 
 ## 1. Read HTML from page containing cable news transcript ---------------
 
 page <- read_html('https://www.foxnews.com/transcript/tucker-the-us-is-looking-at-a-grim-economic-picture')
 
+## 2. Keep only the text in the main body ------------
+
 text <- page |>
-  html_elements('body') |>
-  html_text2(preserve_nbsp = FALSE)
+  # get the paragraphs (p)
+  html_elements('p') |>
+  # convert to text
+  html_text2()
 
-d <- tibble(text)
-
-library(tidytext)
-
-d2 <- d |>
+# looks like the transcript is in the fourth paragraph element
+# let's take it and convert it to a tokenized dataframe
+d <- tibble(transcript = text[4]) |>
   unnest_tokens(output = 'word',
-                input = 'text')
+                input = 'transcript')
+
+# create a word cloud
+library(wordcloud2)
+
+# word cloud of all the sentiment-laden words
+d |>
+  anti_join(get_stopwords()) |>
+  inner_join(get_sentiments('bing')) |>
+  filter(!(word %in% c('right', 'like', 'well', 'trump'))) |>
+  count(word) |>
+  wordcloud2()
