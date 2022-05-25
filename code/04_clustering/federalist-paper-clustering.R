da1d1ac9cd61b5351b27e70d5ad7402a343f7eb3
joeornstein <jornstein@gmail.com>
2022-05-25 13:09:27 -0400

    find words that distinguish the clusters

--- code/04_clustering/federalist-paper-clustering.R
+++ code/04_clustering/federalist-paper-clustering.R
@@ -50,13 +50,36 @@ df <- corpus::federalist |>
   select(name, title, author, text) |>
   left_join(cluster_assignments, by = 'name')
 
-## Step 3: Try to find distinctive words ------------------------
+## Step 3: Try to find words that distinguish one cluster from another ------------------------
+
+cluster_of_interest <- 8
+
+distinctive_words <- tidy_federalist |>
+  left_join(cluster_assignments, by = 'name') |>
+  mutate(cluster_of_interest = if_else(cluster == cluster_of_interest,
+                                       'yes', 'no')) |>
+  # count the words in each cluster
+  group_by(cluster_of_interest, word) |>
+  summarize(n = sum(n)) |>
+  pivot_wider(names_from = 'cluster_of_interest',
+              values_from = 'n',
+              values_fill = 0) |>
+  # compute word shares
+  mutate(yes = yes / sum(yes),
+         no = no / sum(no)) |>
+  mutate(delta = yes - no) |>
+  arrange(-delta)
+
+
+# now with center vectors
 
 centers <- km$centers
 
 # take the vector for one topic and subtract the average vector for all the other topics
-v1 <- centers[4,]
-v2 <- colMeans(centers[c(1:3, 5:10),])
+cluster_of_interest <- 10
+
+v1 <- centers[cluster_of_interest,]
+v2 <- colMeans(centers[-cluster_of_interest,])
 
 # subtract the two and sort
 distinctive_words <- sort(v1 - v2)
