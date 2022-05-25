c37df1275e0894eb27caa6e07cf1cfee33f8986c
joeornstein <jornstein@gmail.com>
2022-05-25 16:26:52 -0400

    update my cluster decisions

--- code/04_clustering/federalist-paper-clustering.R
+++ code/04_clustering/federalist-paper-clustering.R
@@ -19,16 +19,19 @@ tidy_federalist <- corpus::federalist |>
   count(name, word) |>
   # remove the numerals
   filter(str_detect(word, '[0-9]', negate = TRUE)) |>
-  # turn the counts into percentages
-  group_by(name) |>
-  mutate(word_share = n / sum(n))
+  # compute tf and tf-idf
+  bind_tf_idf(term = 'word',
+              document = 'name',
+              n = 'n') |>
+  # filter by frequency. remove super rare and super common words
+  filter(n > 1, idf != 0)
 
 
 # convert to a document-term matrix
 federalist_dtm <- cast_dtm(data = tidy_federalist,
                            document = 'name',
                            term = 'word',
-                           value = 'word_share')
+                           value = 'tf')
 
 federalist_dtm
 
@@ -36,7 +39,7 @@ federalist_dtm
 ## step 2: k-means clustering -------------------------
 
 km <- kmeans(x = federalist_dtm,
-             centers = 10,
+             centers = 7,
              nstart = 100)
 
 km$cluster
@@ -50,44 +53,25 @@ df <- corpus::federalist |>
   select(name, title, author, text) |>
   left_join(cluster_assignments, by = 'name')
 
+count(df, cluster)
+
 ## Step 3: Try to find words that distinguish one cluster from another ------------------------
 
-cluster_of_interest <- 8
+cluster_of_interest <- 5
 
-distinctive_words <- tidy_federalist |>
+tidy_federalist |>
   left_join(cluster_assignments, by = 'name') |>
-  mutate(cluster_of_interest = if_else(cluster == cluster_of_interest,
-                                       'yes', 'no')) |>
+  mutate(in_cluster = if_else(cluster == cluster_of_interest,
+                                       'within_cluster', 'outside_cluster')) |>
   # count the words in each cluster
-  group_by(cluster_of_interest, word) |>
+  group_by(in_cluster, word) |>
   summarize(n = sum(n)) |>
-  pivot_wider(names_from = 'cluster_of_interest',
+  pivot_wider(names_from = 'in_cluster',
               values_from = 'n',
               values_fill = 0) |>
   # compute word shares
-  mutate(yes = yes / sum(yes),
-         no = no / sum(no)) |>
-  mutate(delta = yes - no) |>
-  arrange(-delta)
-
-
-# now with center vectors
-
-centers <- km$centers
-
-# take the vector for one topic and subtract the average vector for all the other topics
-cluster_of_interest <- 10
-
-v1 <- centers[cluster_of_interest,]
-v2 <- colMeans(centers[-cluster_of_interest,])
-
-# subtract the two and sort
-distinctive_words <- sort(v1 - v2)
-
-# ten most distinctive words from that cluster
-distinctive_words[8490:8500]
-
-# next step: dive into the texts and see if this would be a good cluster label
-
-
-
+  mutate(within_cluster = within_cluster / sum(within_cluster),
+         outside_cluster = outside_cluster / sum(outside_cluster)) |>
+  mutate(delta = within_cluster - outside_cluster) |>
+  arrange(-delta) |>
+  head(10)
