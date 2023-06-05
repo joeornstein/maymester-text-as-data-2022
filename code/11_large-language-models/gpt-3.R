5382140192f5cfb8856d7188f571886929e7a728
Joe <jornstein@gmail.com>
2023-06-05 15:55:09 -0400

    gpt-3

---
+++ code/11_large-language-models/gpt-3.R
@@ -0,0 +1,28 @@
+
+
+
+# to install the package
+# devtools::install_github('joeornstein/text2data')
+
+library(tidyverse)
+library(text2data)
+
+# let's read in our API key like this
+# get the API key from https://platform.openai.com/account/api-keys
+api_key <- read_file('openai-api-key.txt')
+
+# then setup the OpenAI Python module like this
+text2data::setup_openai()
+
+
+complete_prompt('My favorite food is', max_tokens = 30)
+
+complete_prompt('My favorite food is', max_tokens = 1)
+
+
+
+complete_prompt('Decide whether the sentiment of the following statment is Positive or Negative\n\n"Help I am stuck in a well."\n\nSentiment:', max_tokens = 1)
+
+
+
+
